3a4d35c013cc451f2753bde97babd43ffe511815
maxor5991 <aljoseguba1995@gmail.com>
2017-11-27 13:06:38 -0500

    LOL antes de irme

--- MyApplication2/app/src/main/java/com/example/aljos/myapplication/Adapters/AdapterHisto.java
+++ MyApplication2/app/src/main/java/com/example/aljos/myapplication/Adapters/AdapterHisto.java
@@ -10,20 +10,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.aljos.myapplication.Data.DataFish;
+import com.example.aljos.myapplication.Data.DataHistorias;
 import com.example.aljos.myapplication.R;
 
 import java.util.Collections;
 import java.util.List;
 
-public class AdapterFish extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+/**
+ * Created by Maxor on 27/11/17.
+ */
+
+public class AdapterHisto extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context context;
     private LayoutInflater inflater;
-    List<DataFish> data= Collections.emptyList();
-    DataFish current;
+    List<DataHistorias> data= Collections.emptyList();
+    DataHistorias current;
     int currentPos=0;
 
-    public AdapterFish(Context context, List<DataFish> data){
+    public AdapterHisto(Context context, List<DataHistorias> data){
         this.context=context;
         inflater= LayoutInflater.from(context);
         this.data=data;
@@ -31,20 +37,18 @@ public class AdapterFish extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view=inflater.inflate(R.layout.container_fish, parent,false);
-        MyHolder holder=new MyHolder(view);
+        View view=inflater.inflate(R.layout.container_histo, parent,false);
+        AdapterHisto.MyHolder holder=new AdapterHisto.MyHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
 
-        MyHolder myHolder= (MyHolder) holder;
-        DataFish current=data.get(position);
-        myHolder.textFishName.setText(current.fishName);
-        myHolder.textSize.setText("Size: " + current.sizeName);
-        myHolder.textType.setText("Marca: " + current.catName);
-        myHolder.textPrice.setText("$" + current.price);
+        AdapterHisto.MyHolder myHolder= (AdapterHisto.MyHolder) holder;
+        DataHistorias current=data.get(position);
+        myHolder.textID.setText(current.idHisto);
+        myHolder.textType.setText("Marca: " + current.desHisto);
         myHolder.textPrice.setTextColor(ContextCompat.getColor(context, R.color.colorAccent));
 
         // load image into imageview using glide
@@ -57,26 +61,6 @@ public class AdapterFish extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return data.size();
+        return 0;
     }
-    class MyHolder extends RecyclerView.ViewHolder{
-
-        TextView textFishName;
-        ImageView ivFish;
-        TextView textSize;
-        TextView textType;
-        TextView textPrice;
-
-        // create constructor to get widget reference
-        public MyHolder(View itemView) {
-            super(itemView);
-            textFishName= (TextView) itemView.findViewById(R.id.textFishName);
-            ivFish= (ImageView) itemView.findViewById(R.id.ivFish);
-            textSize = (TextView) itemView.findViewById(R.id.textSize);
-            textType = (TextView) itemView.findViewById(R.id.textType);
-            textPrice = (TextView) itemView.findViewById(R.id.textPrice);
-        }
-
-    }
-
 }
---
+++ MyApplication2/app/src/main/java/com/example/aljos/myapplication/Data/DataHistorias.java
@@ -0,0 +1,10 @@
+package com.example.aljos.myapplication.Data;
+
+/**
+ * Created by Maxor on 27/11/17.
+ */
+
+public class DataHistorias {
+    public String idHisto;
+    public String desHisto;
+}
